3b6b60982185e013b6a0235b16754a4106a79c3e
marcruedaa <marcrueda2006@ginebro.cat>
2025-01-24 15:15:19 +0100

    Primer Commit

---
+++ ruedaGrupCEnunciat1.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    nomServei1 = "B - Bàsic: Menú senzill i sala"
+    nomServei2 = "P - Prèmium: Menú gurmet i sala gran"
+    nomServei3 = "V - VIP: Menú exclusiu, sala gran i decoració especial"
+    nomServei4 = "T - Total VIP: Inclou tots els serveis VIP més assistència especialitzada"
+    numEmpreses=int()
+
+    preuDelServei1 = 30
+    preuDelServei2 = 50
+    preuDelServei3 = 80
+    preuDelServei4 = 120
+
+    print("De quantes empreses vols demanar les dades?(màxim 10)", end="")
